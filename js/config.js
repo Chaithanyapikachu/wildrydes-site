85735f3e8ea23555e2dc1c5cf4376f8ec233a6d9
chaithanya <93250351+Chaithanyapikachu@users.noreply.github.com>
2025-02-11 19:04:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_SuLYJluRL', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '30a8ubliu3s20bek07k4vru4na', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        Clientsecret: '5tl1il07qm74n4vofpagr1chheln6453qmmiiv31unkfqe6rs2f'
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
